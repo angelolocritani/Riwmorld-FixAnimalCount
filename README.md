a6c2cb34426f778274413d35c48b3869ad37e421
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2022-12-18 10:26:13 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Riwmorld-FixAnimalCount
- 
+
+[![Version](https://img.shields.io/badge/Rimworld-1.4-green.svg)](http://rimworldgame.com/)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/angelolocritani/Rimworld-FixAnimalCount)](https://github.com/angelolocritani/Rimworld-FixAnimalCount/releases/latest)
+# FixAnimalCount
+
+ FixAnimalCount mod for Rimworld
+
+
+Fixes a glitch on the auto-slaughter total animals column.
+In vanilla auto-slaughter based on total number is only perfomed using allowed slaughterable animals
+Example:
+total limit = 8
+males = 5
+pregnant females = 3
+non-pregnant females = 2
+slaughter pregnant = OFF
+total (as displayed in GUI) = male + pregnant females + non-pregnant females = 10
+
+even if 10 > 8, no slaughter occurs because internally the "real" calculation considers only 5 males + 2 non-pregnant females = total 7
+
+this mod fixes the value of the total count, considering pregnant females only if the specifich check is toggled: it alligns the logic to how bonded animal are managed. Indeed if "slaughter bonded" is NOT check, they are NOT counted in total column.
+
---
+++ Source/Rimworld_FixAnimalCount/FixAnimalCount.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using System.Reflection;
+using HarmonyLib;
+using System;
+
+namespace Rimworld_FixAnimalCount
+{
+
+    [StaticConstructorOnStartup]
+    public static class FixAnimalCountMod
+    {
+        public static Harmony harmonyInstance;
+
+
+
+        static FixAnimalCountMod()
+        {
+            Log.Message("loaded");
+            harmonyInstance = new Harmony("arl85.FixAnimalCount");
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+            harmonyInstance.PatchAll();
+
+        }
+
+
+        [HarmonyPatch(typeof(Dialog_AutoSlaughter), "CountPlayerAnimals")] // new Type[] { typeof(Map), typeof(AutoSlaughterConfig), typeof(ThingDef), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int), typeof(int) }, new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out, ArgumentType.Out })]
+        public static class Dialog_AutoSlaughter_CountPlayerAnimals
+        {
+            static void Postfix(int currentMales, int currentMalesYoung, int currentFemales, int currentFemalesYoung, ref int currentTotal)
+            {
+                currentTotal = currentMales + -currentMalesYoung + currentFemales + currentFemalesYoung;
+            }
+
+        }
+
+    }
+}
+
